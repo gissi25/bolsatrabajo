65ec1c556e362e53cd6456328c160bebb090aa6e
Karla Gissell Girón Martinez <gm23025@ues.edu.sv>
2026-05-09 20:20:29 -0600

    ajuste de libreria en main

--- app/src/main/java/sv/ues/fia/eisi/bt/MainActivity.kt
+++ app/src/main/java/sv/ues/fia/eisi/bt/MainActivity.kt
@@ -3,7 +3,7 @@ package sv.ues.fia.eisi.bt
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragmento
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
 
